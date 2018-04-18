3fb44bdbec6cefa9c8817a40eb53c2feeda2d9df
Harold <hhausman@gmail.com>
2018-04-18 15:49:00 -0600

    [core] First cut at groups.

--- src/cljs/midi_cc_randomizer/core.cljs
+++ src/cljs/midi_cc_randomizer/core.cljs
@@ -4,7 +4,12 @@
 (enable-console-print!)
 
 (defonce state* (r/atom {:midi nil
-                         :parameters []}))
+                         :parameters (vec (repeat 0x80 nil))
+                         :group-colors ["rgb(230,159,0)"
+                                        "rgb(86,180,233)"
+                                        "rgb(0,158,115)"
+                                        "rgb(204,121,167)"]
+                         :selected-group-index 0}))
 
 (defn maplike->map
   [maplike]
@@ -20,39 +25,74 @@
    (cond
      (:error-message @state*) [:div "ERROR: " (:error-message @state*)]
      (nil? (:midi @state*)) [:div "Loading..."]
-     true (let [output-map (maplike->map (.-outputs (:midi @state*)))]
-            (if-not (pos? (count output-map))
-              [:div "No MIDI output ports found. :("]
-              [:div
-               [:h3 "MIDI output ports:"]
-               (into [:select#port]
-                     (for [[k v] output-map]
-                       [:option {:value k} (.-name v)]))
-               [:div.parameter-area
-                [:h3 "Parameters:"]
-                (into [:div.parameters]
-                      (for [{:keys [cc min max]} (:parameters @state*)]
-                        [:div
-                         [:tt (str "CC: 0x" (.toString cc 16) " Min: " min " Max: " max " ")]
-                         [:button {:on-click (fn [e]
-                                               (let [dropdown (js/document.getElementById "port")
-                                                     output (get output-map (.-value dropdown))]
-                                                 (let [a (array 0xB0 cc (+ min (rand-int (inc (- max min)))))]
-                                                   (.send output a))))}
-                          "LEARN"]]))
-                [:button {:on-click (fn [e]
-                                      (let [parameter-count (count (:parameters @state*))]
-                                        (swap! state* update :parameters conj {:cc (+ 0x66 parameter-count)
-                                                                               :min 0x00
-                                                                               :max 0x7F})))}
-                 "ADD"]]
-               [:h3 "Go:"]
-               [:div [:button {:on-click (fn [e]
-                                           (let [dropdown (js/document.getElementById "port")
-                                                 output (get output-map (.-value dropdown))]
-                                             (doseq [{:keys [cc min max]} (:parameters @state*)]
-                                               (.send output (array 0xB0 cc (+ min (rand-int (inc (- max min)))))))))}
-                      "SEND"]]])))])
+     :else (let [output-map (maplike->map (.-outputs (:midi @state*)))]
+             (if-not (pos? (count output-map))
+               [:div "Midi connected, but no output ports found. :("]
+               [:div
+                [:div.output-port-area
+                 [:h3 "MIDI output ports:"]
+                 (into [:select#port]
+                       (for [[k v] output-map]
+                         [:option {:value k} (.-name v)]))]
+                [:div.group-area
+                 [:h3 "Groups"]
+                 [:div {:style {:margin-bottom "4px"}}
+                  [:button {:on-click (fn [e]
+                                        (let [dropdown (js/document.getElementById "port")
+                                              output (get output-map (.-value dropdown))]
+                                          (doseq [cc (range 0x80)]
+                                            (.send output (array 0xB0 cc (rand-int 0x80))))))}
+                   "SEND ALL"]
+                  " "
+                  [:button {:on-click (fn [e]
+                                        (let [dropdown (js/document.getElementById "port")
+                                              output (get output-map (.-value dropdown))]
+                                          (doseq [cc (range 0x80)]
+                                            (when (get-in @state* [:parameters cc :group-index])
+                                              (.send output (array 0xB0 cc (rand-int 0x80)))))))}
+                   "SEND ALL GROUPED"]]
+                 (into [:div.groups]
+                       (for [[i c] (map-indexed vector (:group-colors @state*))]
+                         [:div.group
+                          [:span {:style {:vertical-align :middle
+                                          :display :inline-block
+                                          :width "2em" :height "2em" :background c
+                                          :border (if (= i (:selected-group-index @state*))
+                                                    "solid 2px black"
+                                                    "solid 2px white")}
+                                  :on-click (fn [e] (swap! state* assoc :selected-group-index i))}
+                           " "]
+                          " "
+                          [:button {:on-click (fn [e]
+                                                (let [dropdown (js/document.getElementById "port")
+                                                      output (get output-map (.-value dropdown))]
+                                                  (doseq [cc (range 0x80)]
+                                                    (when (= i (get-in @state* [:parameters cc :group-index]))
+                                                      (.send output (array 0xB0 cc (rand-int 0x80)))))))}
+                           "SEND"]]))]
+                [:div.parameter-area
+                 [:h3 "Parameters:"]
+                 [:table {:style {:border-collapse :collapse}}
+                  [:thead]
+                  (into [:tbody]
+                        (for [row-index (range 32)]
+                          (into [:tr]
+                                (for [col-index (range 4)]
+                                  (let [cc (+ col-index (* 4 row-index))]
+                                    [:td {:style (merge
+                                                  {:padding-bottom "3px"
+                                                   :padding-right (if (not= 3 col-index) "8px" "0px")
+                                                   :cursor :pointer}
+                                                  (when-let [idx (get-in @state* [:parameters cc :group-index])]
+                                                    {:background (get-in @state* [:group-colors idx])}))}
+                                     [:tt {:on-click (fn [e] (swap! state* assoc-in [:parameters cc] {:group-index (:selected-group-index @state*)}))}
+                                      (str "0x" (if (<= cc 0xf) "0" "") (.toString cc 16))]
+                                     " "
+                                     [:button {:on-click (fn [e]
+                                                           (let [dropdown (js/document.getElementById "port")
+                                                                 output (get output-map (.-value dropdown))]
+                                                             (.send output (array 0xB0 cc (rand-int 0x80)))))}
+                                      "TAP"]])))))]]])))])
 
 (defn- on-midi
   [midi]
